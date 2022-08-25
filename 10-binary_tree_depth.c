49d607e9a55a7ed97fc080bc04a1978665b0ee2f
Tesh2714 <teas38mum@gmail.com>
2022-08-25 05:17:58 -0700

    10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,21 @@
+include "binary_trees.h"
+
+/**
+ * binary_tree_depth - depth of a node in a binary tree
+ * @node: pointer to the node 
+ * 
+ * Return: depth of a node in a binary tree. If node is NULL, return 0
+ */
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+	size_t cnt = 0;
+
+	if (node == NULL)
+		return (cnt);
+	while (node->parent != NULL)
+	{
+		cnt++;
+		node = node->parent;
+	}
+	return (cnt);
+}
