f9a5b1a95425dcda5edce98eb38bfaeb84eac171
Tesh2714 <teas38mum@gmail.com>
2022-08-25 05:22:09 -0700

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,8 +2,7 @@
 
 /**
  * binary_tree_depth - depth of a node in a binary tree
- * @node: pointer to the node 
- * 
+ * @node: pointer to the node
  * Return: depth of a node in a binary tree. If node is NULL, return 0
  */
 size_t binary_tree_depth(const binary_tree_t *node)
